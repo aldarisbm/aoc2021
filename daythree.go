c587aadfa02a5fe7d554190fd65216be42b18c5e
Jose Berrio <aldarisbm@gmail.com>
2021-12-05 21:09:28 -0500

    thinking through 3rd day

--- daythree.go
+++ daythree.go
@@ -1,5 +1,39 @@
 package main
 
-func getDayThreeResult() (int, int, error) {
+import (
+	"strconv"
+	"strings"
+)
 
+func getDayThreeResult() (int, int, error) {
+	input, err := getDayInput(3)
+	if err != nil {
+		return 0, 0, err
+	}
+	inputValues, err := getDayThreeValues(input)
+	if err != nil {
+		return 0, 0, err
+	}
+	firstResult := getPowerConsumption(inputValues)
+	return firstResult, 0, nil
+}
+
+func getDayThreeValues(input string) ([]int, error) {
+	inputSplit := strings.Split(input, "\n")
+	var intArray []int
+	for _, element := range inputSplit {
+		if len(element) > 0 {
+			castValue, err := strconv.Atoi(element)
+			if err != nil {
+				return nil, err
+			}
+			intArray = append(intArray, castValue)
+		}
+	}
+	return intArray, nil
+}
+
+func getPowerConsumption(input []int) int {
+
+	return 0
 }
